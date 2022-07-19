420274bff1b5c6a859e33955d20f17de27a4f6e1
Seohyeon <0205lucy@naver.com>
2022-07-19 19:53:46 +0900

    :tada: 시작

---
+++ chrome_momentum/app.js
@@ -0,0 +1,26 @@
+// const calculator = {
+//     add: function (a,b) {
+//         console.log(a + b);
+//     },
+
+//     divide: function(a,b){
+//         console.log(a/b)
+//     }
+// }
+
+// calculator.add(10,2);  
+// calculator.divide(20,4);
+
+const calculator = {
+    add: function (a,b) {
+        return a + b;
+    },
+
+    divide: function(a,b){
+        return a / b 
+    }
+}
+
+calculator.add(10,2);  
+calculator.divide(20,4);
+
